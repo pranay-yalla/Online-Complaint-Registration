7444fdada48c4984519949125735a6428eacf0be
pranay-yalla <pranayyalla06@gmail.com>
2026-07-13 19:06:28 +0530

    Update messageRoutes.js

--- backend/routes/messageRoutes.js
+++ backend/routes/messageRoutes.js
@@ -1,1 +1,13 @@
 
+const express = require("express");
+const router = express.Router();
+
+const {
+  sendMessage,
+  getMessages,
+} = require("../controllers/messageController");
+
+router.post("/messages", sendMessage);
+router.get("/messages/:complaintId", getMessages);
+
+module.exports = router;
