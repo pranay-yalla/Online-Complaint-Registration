bb0486c72616900565b709718f4f448716e43e60
pranay-yalla <pranayyalla06@gmail.com>
2026-07-13 18:58:51 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,7 @@
+const mongoose = require("mongoose");
 
-const mongoose = require("mongoose")
-
-mongoose.connect("mongodb://127.0.0.1:27017/details")
-.then(()=>{
-   console.log("connected to mongodb")
-})
+mongoose
+  .connect("mongodb://127.0.0.1:27017/details")
+  .then(() => {
+    console.log("connected to mongodb");
+  });
