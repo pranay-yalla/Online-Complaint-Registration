24aa8172cd5393399b5a8dea59b81894e796efbe
pranay-yalla <pranayyalla06@gmail.com>
2026-07-13 19:08:04 +0530

    Update userRoutes.js

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,1 +1,23 @@
 
+const express = require("express");
+const router = express.Router();
+
+const {
+  getAgentUsers,
+  getOrdinaryUsers,
+  getAgentById,
+  deleteOrdinaryUser,
+  updateUser,
+} = require("../controllers/userController");
+
+router.get("/AgentUsers", getAgentUsers);
+
+router.get("/OrdinaryUsers", getOrdinaryUsers);
+
+router.get("/AgentUsers/:agentId", getAgentById);
+
+router.delete("/OrdinaryUsers/:id", deleteOrdinaryUser);
+
+router.put("/user/:_id", updateUser);
+
+module.exports = router;
