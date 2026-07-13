e88112a0a3e7462c1e9dffe6961034e11b2ae80b
pranay-yalla <pranayyalla06@gmail.com>
2026-07-13 19:11:55 +0530

    Update complaintRoutes.js

--- backend/routes/complaintRoutes.js
+++ backend/routes/complaintRoutes.js
@@ -1,1 +1,26 @@
 
+const express = require("express");
+const router = express.Router();
+
+const {
+  createComplaint,
+  getUserComplaints,
+  getAllComplaints,
+  assignComplaint,
+  getAgentComplaints,
+  updateComplaintStatus,
+} = require("../controllers/complaintController");
+
+router.post("/Complaint/:id", createComplaint);
+
+router.get("/status/:id", getUserComplaints);
+
+router.get("/status", getAllComplaints);
+
+router.post("/assignedComplaints", assignComplaint);
+
+router.get("/allcomplaints/:agentId", getAgentComplaints);
+
+router.put("/complaint/:complaintId", updateComplaintStatus);
+
+module.exports = router;
